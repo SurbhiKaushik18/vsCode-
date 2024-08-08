0b323466d3cb05085b36882c595c3929a9032d1c
SurbhiKaushik18 <sliet.surbhisingh2000@gmail.com>
2024-08-08 21:08:36 +0530

    Practise Question

---
+++ r2.py
@@ -0,0 +1,14 @@
+for row in range(4):
+    for col in range(4):
+        if col==0:
+            print(1, end=" ")
+        elif col==1:
+            print(2, end=" ")
+        elif col==2:
+            print(3, end=" ")
+        elif col==3:
+            print(4, end=" ")
+
+        else:
+            print(end=" ")
+    print()
